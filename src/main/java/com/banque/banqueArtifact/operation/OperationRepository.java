3026277f08bdddcdd1a1476407b7073ba3eb9776
Mélissa Leal <sebriniel@hotmail.fr>
2019-01-07 12:00:59 +0100

    jdbc

--- src/main/java/com/banque/banqueArtifact/operation/OperationRepository.java
+++ src/main/java/com/banque/banqueArtifact/operation/OperationRepository.java
@@ -2,5 +2,7 @@ package com.banque.banqueArtifact.operation;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OperationRepository extends JpaRepository<Operation, Long> {
 }
--- src/main/java/com/banque/banqueArtifact/operation/OperationResource.java
+++ src/main/java/com/banque/banqueArtifact/operation/OperationResource.java
@@ -1,10 +1,15 @@
 package com.banque.banqueArtifact.operation;
 
+import com.banque.banqueArtifact.client.Client;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 public class OperationResource {
@@ -14,5 +19,12 @@ public class OperationResource {
     @GetMapping("/operations")
     public List<Operation> retrieveAllOperations() {
         return operationRepository.findAll();
+    }
+
+    @RequestMapping(value = "/operations/{clientId}", method = RequestMethod.GET)
+    public List<Operation> retrieveAllOperationsByIdClient(@PathVariable @NotNull Long clientId) {
+        String sql = "SELECT o.id, o.id_cpt_debite, o.id_cpt_credite, o.montant, cc.id_client FROM operation o LEFT JOIN compte_courant cc ON operation.id_cpt_credite = cc.numero WHERE id_client = " + clientId;
+        List<Operation> operations = new ArrayList<Operation>();
+        List<Map> rows = getJdbcTemplate().queryForList(sql);
     }
 }
